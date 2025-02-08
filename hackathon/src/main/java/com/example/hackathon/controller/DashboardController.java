ab6974010e3032a85735bd736ab031e134db05a7
Daniel Burrows <db532@student.le.ac.uk>
2025-02-08 15:32:11 +0000

    Prelim graph work

---
+++ hackathon/src/main/java/com/example/hackathon/controller/DashboardController.java
@@ -0,0 +1,12 @@
+package com.example.hackathon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+    @GetMapping("/graphs")
+    public String graphs() {
+        return "graphs";
+    }
+}
